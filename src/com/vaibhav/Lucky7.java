00c6661c3c6af53e25cc37e959a8533b44c13e7d
Vaibhav Ram <vaibhavramp176@gmail.com>
2026-04-06 19:39:34 +0530

    Add Program details and main method

--- src/com/vaibhav/Lucky7.java
+++ src/com/vaibhav/Lucky7.java
@@ -1,4 +1,17 @@
+/*
+    Author: VAIBHAV
+    Place: Delhi, India
+    Date: 04/04/2026
+    Last Edited:
+    Description: This program will help you to play lucky7 game. In this game, you will be in a virtual casino with an initial amount. Now you will bet an amount and then two dice will roll. If the sum of dice's result is equal to 7, you will win, otherwise lose. If win's you will be awarded by three times of your bet amount. But if lose you will lose all your bet amount.
+    Previous Improvements:
+    Future Improvements:
+*/
+
 package com.vaibhav;
 
 public class Lucky7 {
+    public static void main(String[] args) {
+
+    }
 }
